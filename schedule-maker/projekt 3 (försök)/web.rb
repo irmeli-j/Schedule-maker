8167a52771bdc3554d39671fc519b2027b6163af
irmeli-johansson-nti-johanneberg <94969179+irmeli-johansson-nti-johanneberg@users.noreply.github.com>
2022-06-06 17:09:34 +0200

    Delete schedule-maker directory

--- schedule-maker/projekt 3 (försök)/web.rb
+++
@@ -1,8 +0,0 @@
-#detta är jag helt säker på att det är fel men hinner inte sätta mig in i hur man faktiskt ska göra. 
-#Ska på något vis kunna koppla slim filen till ruby koden. (tror jag lol)
-require "sinatra"
-require "slim"
-
-get "/" do
-  slim :layout
-end
